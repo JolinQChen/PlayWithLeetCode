fb426ab204bd5bfdead03c91e17d2d283c0c112e
JolinQChen <qiaolingchen1@gmail.com>
2020-05-17 16:38:42 -0400

    2020.5.17 Trajan Algorithm

---
+++ src/DFS_BFS/_1192_Critical_Connections_in_a_Network.java
@@ -0,0 +1,117 @@
+package DFS_BFS;
+import sun.awt.image.ImageWatched;
+
+import java.util.*;
+
+/**There are n servers numbered from 0 to n-1 connected by undirected server-to-server connections
+ * forming a network where connections[i] = [a, b] represents a connection between servers a and b.
+ * Any server can reach any other server directly or indirectly through the network.
+ *
+ * A critical connection is a connection that, if removed, will make some server unable to reach
+ * some other server.
+ *
+ * Return all critical connections in the network in any order.
+ *
+ * Example:
+ * Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+ * Output: [[1,3]]
+ * Explanation: [[3,1]] is also accepted.
+ * */
+
+/** 经典的Trajan算法 */
+
+public class _1192_Critical_Connections_in_a_Network {
+
+    private int times;
+    private int[] parent;
+    private Map<Integer, List<Integer>> graphMap; //Map表示graph: NodeIndex -> List (following nodes)
+    private int[] low;
+    private int[] dfn;
+    private List<List<Integer>> res; //添加最终结果
+
+
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        /** Initialization */
+        times = 1;
+        parent = new int[n];
+        Arrays.fill(parent,-1);
+        low = new int[n];
+        dfn = new int[n];
+        res = new LinkedList<>();
+
+        /** construct graph*/
+        graphMap = new HashMap<>();
+        for(List<Integer> list: connections){
+            List<Integer> tmp0 = graphMap.getOrDefault(list.get(0),new LinkedList<>());
+            List<Integer> tmp1 = graphMap.getOrDefault(list.get(1),new LinkedList<>());
+            tmp0.add(list.get(1));
+            tmp1.add(list.get(0));
+            graphMap.put(list.get(0), tmp0);
+            graphMap.put(list.get(1),tmp1);
+        }
+
+        /** begin dfs */
+        for(int i=0; i<n; i++){
+            if(dfn[i]==0){
+                dfs(i);
+            }
+        }
+        return res;
+
+    }
+
+    public void dfs(int x){
+        // 对x node进行dfs遍历并判定桥，将最终判定结果加入res中
+        dfn[x] = low[x] = times++;
+        List<Integer> list = graphMap.get(x);
+        for(int y:list){
+            if(dfn[y]==0){
+                //y并没有被访问过
+                parent[y] = x;
+                dfs(y);
+                //开始判定
+                if(low[y]>dfn[x]){
+                    //满足桥的要求
+                    List<Integer> tmp = new LinkedList<>();
+                    tmp.add(x);
+                    tmp.add(y);
+                    res.add(tmp);
+                }
+                low[x] = Math.min(low[x],low[y]);//更新父节点的low
+            }
+            else if(y!=parent[x]){
+                //y已经被访问过，但是y不是x的father
+                low[x] = Math.min(low[y],low[x]); //更新
+            }
+        }
+    }
+    /*
+    public static void main(String[] args){
+
+        List<List<Integer>> input = new LinkedList<>();
+        int n=4;
+        List<Integer> input1 = new LinkedList<>();
+        input1.add(0);input1.add(1);
+        input.add(input1);
+
+        List<Integer> input2 = new LinkedList<>();
+        input2.add(1);input2.add(2);
+        input.add(input2);
+
+        List<Integer> input3 = new LinkedList<>();
+        input3.add(2);input3.add(0);
+        input.add(input3);
+
+        List<Integer> input4 = new LinkedList<>();
+        input4.add(1);input4.add(3);
+        input.add(input4);
+
+        _1192_Critical_Connections_in_a_Network test = new _1192_Critical_Connections_in_a_Network();
+        List<List<Integer>> test_res = test.criticalConnections(n, input);
+        for(List<Integer> list:test_res){
+            for(int t:list) System.out.print(t + "\t");
+            System.out.println();
+        }
+    }
+    */
+}
---
+++ src/_1192_Critical_Connections_in_a_Network.java
@@ -0,0 +1,96 @@
+import sun.awt.image.ImageWatched;
+
+import java.util.*;
+// 考察Trajan Algorithm
+/**
+ * 有 n 台服务器，分别按从 0 到 n-1 的方式进行了编号。
+ *
+ * 它们之间以「服务器到服务器」点对点的形式相互连接组成了一个内部集群，其中连接 connections 是无向的。
+ *
+ * 从形式上讲，connections[i] = [a, b] 表示服务器 a 和 b 之间形成连接。任何服务器都可以直接
+ * 或者间接地通过网络到达任何其他服务器。
+ *
+ * 「关键连接」是在该集群中的重要连接，也就是说，假如我们将它移除，便会导致某些服务器无法访问其他服务器。
+ *
+ * 请你以任意顺序返回该集群内的所有 「关键连接」。
+ *
+ * 输入：n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+ * 输出：[[1,3]]
+ * 解释：[[3,1]] 也是正确的。
+ *
+ * */
+
+public class _1192_Critical_Connections_in_a_Network {
+    /**
+     * Trajan Algorithm
+     * https://zhuanlan.zhihu.com/p/101923309
+     * 解题思路
+     * 如果一个边是关键路径，当且仅当该边不在环中。
+     *
+     * 使用深度优先搜索查找环。
+     *
+     * Build a graph first
+     *
+     * DFS each node, find lowest timestamp for each nested child nodes during backtracking
+     * If lowest timestamp is still larger after backtracking means it is not part of
+     * the cricle, then it is critical connection.
+     *
+     * */
+    private int time=1; //设置一个时间戳，从1开始计数
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+
+        /**Initialization*/
+        List<List<Integer>> res = new ArrayList<>();
+        Map<Integer, List<Integer>> graphMap = new HashMap<>();
+
+
+        for (List<Integer> connection : connections) {
+            addConnectionToGraph(graphMap, connection.get(0), connection.get(1));
+            addConnectionToGraph(graphMap, connection.get(1), connection.get(0));
+        }
+        int[] times = new int[n]; // timestamp of traversing order during DFS
+        int[] lows = new int[n]; // lowest time current node can get
+        int[] parents = new int[n]; // record parent of each node, so child won't go to parent in DFS
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (times[i] > 0) continue; // means visited
+            dfs(i, graphMap, times, lows, parents, res);
+        }
+        return res;
+    }
+    private void dfs(int node, Map<Integer, List<Integer>> map, int[] times, int[] lows, int[] parents, List<List<Integer>> res) {
+        // base cases
+        if (times[node] > 0) return; // visited before
+
+        lows[node] = times[node] = time++;
+
+        // recursive rule
+        for (int child : map.get(node)) {
+            if (parents[node] == child) continue; // child never goes to it's parent again
+            if (times[child] > 0 && child != parents[node]) { // child never goes to it's parent again
+                lows[node] = Math.min(lows[node], lows[child]);
+            }
+            if (times[child] > 0) continue;
+
+            parents[child] = node;
+
+            dfs(child, map, times, lows, parents, res);
+            if(lows[child] > times[node]) { // critical connection, not part of the circle
+                // record critical connection
+                List<Integer> list = new ArrayList<>();
+                list.add(node);
+                list.add(child);
+                res.add(list);
+            }
+            lows[node] = Math.min(lows[node], lows[child]); // back tracking, updating the lowest time
+        }
+    }
+
+    private void addConnectionToGraph(Map<Integer, List<Integer>> graphMap, int node1, int node2) {
+        List<Integer> neighbours = graphMap.getOrDefault(node1, new ArrayList<>());
+        neighbours.add(node2);
+        graphMap.put(node1, neighbours);
+    }
+}
