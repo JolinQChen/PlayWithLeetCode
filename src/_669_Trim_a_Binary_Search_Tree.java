859f38408c98d0c248416aa1f321e6b086201788
JolinQChen <qiaolingchen1@gmail.com>
2020-05-24 16:38:19 -0400

    2020.5.24

--- src/_669_Trim_a_Binary_Search_Tree.java
+++ src/_669_Trim_a_Binary_Search_Tree.java
@@ -42,5 +42,19 @@ import java.util.*;
  * */
 
 public class _669_Trim_a_Binary_Search_Tree {
+    /**
+     * 当 node.val > R，那么修剪后的二叉树必定出现在节点的左边。
+     *
+     * 类似地，当 node.val < L，那么修剪后的二叉树出现在节点的右边。否则，我们将会修剪树的两边。
+     * */
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root==null) return root;
+        if(root.val>R) return trimBST(root.left, L, R);
+        if(root.val<L) return trimBST(root.right,L,R);
 
+        //root处在L,R之间
+        root.left = trimBST(root.left,L,R);
+        root.right = trimBST(root.right,L,R);
+        return root;
+    }
 }
