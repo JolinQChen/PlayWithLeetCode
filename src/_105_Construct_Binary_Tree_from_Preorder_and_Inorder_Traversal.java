e95c46eeaa8705d45cb71fd2a48b18b1e02b83b2
JolinQChen <qiaolingchen1@gmail.com>
2020-05-31 17:21:37 -0400

    2020.5.31

---
+++ src/_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class _105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
+    // start from the first preorder element
+    private int pre_idx = 0;
+    private int[] preorder;
+    private int[] inorder;
+    HashMap<Integer, Integer> idx_map = new HashMap<Integer, Integer>();
+
+    private TreeNode helper(int in_left, int in_right) {
+        if(in_left == in_right) return null;
+        // pick preorder pre_idxth element as root
+        int rootVal = preorder[pre_idx];
+        TreeNode cur_root = new TreeNode(rootVal);
+        // splite inorder list to left and right based on root value
+        int cur_idx = idx_map.get(rootVal);
+        pre_idx++;
+        //left
+        cur_root.left = helper(in_left,cur_idx);
+        cur_root.right = helper(cur_idx+1, in_right);
+        return cur_root;
+    }
+
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+        int idx = 0;
+        for(int val:inorder) idx_map.put(val,idx++);
+        return helper(0, inorder.length);
+    }
+}
---
+++ src/_106_Construct_Binary_Tree_from_Postorder_and_Inorder_Traversal.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class _106_Construct_Binary_Tree_from_Postorder_and_Inorder_Traversal {
+    private int post_index;
+    private int[] inorder;
+    private int[] postorder;
+    private Map<Integer, Integer> map;
+
+    private TreeNode helper(int in_left, int in_right){
+        if(in_left == in_right) return null;
+        int rootVal = postorder[post_index];
+        TreeNode cur = new TreeNode(rootVal);
+        int cur_index = map.get(rootVal);
+
+        post_index--;
+        cur.right = helper(cur_index, in_right);
+        cur.left = helper(in_left, cur_index-1);
+        return cur;
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.postorder = postorder;
+        this.inorder = inorder;
+        this.post_index = postorder.length-1;
+        this.map = new HashMap<>();
+        for(int i=0; i<inorder.length; i++) map.put(inorder[i],i);
+        return helper(-1, inorder.length-1);
+    }
+}
