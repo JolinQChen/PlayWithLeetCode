9ed6179ce1b3319e8dd1ffdb0bcd0375e384b56a
JolinQChen <qiaolingchen1@gmail.com>
2020-06-07 16:57:46 -0400

    2020.6.7

---
+++ src/_894_All_Possible_Full_Binary_Trees.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+public class _894_All_Possible_Full_Binary_Trees {
+    Map<Integer, List<TreeNode>> memo = new HashMap();
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (!memo.containsKey(N)) {
+            List<TreeNode> ans = new LinkedList();
+            if (N == 1) {
+                ans.add(new TreeNode(0));
+            } else if (N % 2 == 1) {
+                for (int x = 0; x < N; ++x) {
+                    int y = N - 1 - x;
+                    for (TreeNode left: allPossibleFBT(x))
+                        for (TreeNode right: allPossibleFBT(y)) {
+                            TreeNode bns = new TreeNode(0);
+                            bns.left = left;
+                            bns.right = right;
+                            ans.add(bns);
+                        }
+                }
+            }
+            memo.put(N, ans);
+        }
+
+        return memo.get(N);
+    }
+
+}
