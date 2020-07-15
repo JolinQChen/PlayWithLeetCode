7b488558e68e88142a710cdc57e5b53bb85c17e1
JolinQChen <qiaolingchen1@gmail.com>
2020-07-15 19:07:36 -0400

    2020.7.15

---
+++ src/_1060_Missing_Element_in_Sorted_Array.java
@@ -0,0 +1,25 @@
+public class _1060_Missing_Element_in_Sorted_Array {
+    public int missingElement(int[] nums, int k)
+    {
+        int[] missing = new int[nums.length];
+        missing[0] = 0;
+        for(int i=1; i<nums.length; i++)
+        {
+            missing[i] = nums[i]-nums[0] - i;
+        }
+        int left = 0;
+        int right = nums.length-1;
+        while(left<right)
+        {
+            int mid = left + (right - left)/2;
+            if(k > missing[mid]) left = mid+1;
+            else if(k<missing[mid]) right = mid;
+            else if(k==missing[mid]) {
+                left = mid;
+                break;
+            }
+        }
+        int gap = missing[left]-k>=0 ? missing[left]-k+1:missing[left]-k;
+        return nums[left]-gap;
+    }
+}
