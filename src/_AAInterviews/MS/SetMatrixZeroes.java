6f4686db76b7bad7326465b0bd26a07323a410a6
JolinQChen <qiaolingchen1@gmail.com>
2020-08-05 12:07:33 -0400

    2020.8.5

--- src/_AAInterviews/MS/SetMatrixZeroes.java
+++ src/_AAInterviews/MS/SetMatrixZeroes.java
@@ -1,4 +1,34 @@
 package _AAInterviews.MS;
-
+import java.util.*;
 public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> col = new HashSet<>();
+        int R = matrix.length;
+        if(R <= 0) return;
+        int C = matrix[0].length;
+        if(C <= 0) return;
+
+
+        for(int r=0; r<R; r++) {
+            for(int c=0; c<C; c++) {
+                if(matrix[r][c] == 0) {
+                    row.add(r);
+                    col.add(c);
+                }
+            }
+        }
+
+        for(int r:row) {
+            for(int c=0;c<C;c++){
+                matrix[r][c] = 0;
+            }
+        }
+
+        for(int c:col) {
+            for(int r=0; r<R; r++) {
+                matrix[r][c] = 0;
+            }
+        }
+    }
 }
