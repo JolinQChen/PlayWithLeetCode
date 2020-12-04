37275d3bbdfe4d54b24ab96b210c9e56a1b7b558
JolinQChen <qiaolingchen1@gmail.com>
2020-12-04 12:17:51 -0500

    2020.12.4

---
+++ src/_All/_137_Single_Number_II.java
@@ -0,0 +1,15 @@
+package _All;
+import java.util.*;
+public class _137_Single_Number_II {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        int res = 0;
+        for(int i:nums) {
+            if(!map.containsKey(i) || map.get(i)==1) {
+                map.put(i, map.getOrDefault(i,0)+1);
+                res ^= i;
+            }
+        }
+        return res;
+    }
+}
